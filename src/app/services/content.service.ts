c439ebefad1e2c61899a7612e37a466cdc6b242f
Zeldoor <mattiarosati98@gmail.com>
2025-07-17 09:46:41 +0200

    random image (dev only)

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -6,8 +6,14 @@ import { Content } from './models/models';
 })
 export class ContentService {
 
-  constructor(){
+  imageUrls: string[] = [
+    'https://random-image-pepebigotes.vercel.app/api/random-image',
+    'https://th.bing.com/th/id/OIP.hHg6ApRWtij6di4UDJJ--AHaLH?w=204&h=306&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+    'https://th.bing.com/th/id/OIP.BFwkigC-Bbm8HW3hddeGQQHaSh?w=115&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    
+  ]
 
+  constructor(){
     this.populateSite(20);
   }
 
@@ -21,12 +27,18 @@ export class ContentService {
       let cont: Content = {
         title: `Titolo ${index}`,
         id: index,
-        imageUrl: index%2 == 0 ? 'https://random-image-pepebigotes.vercel.app/api/random-image' : 'https://th.bing.com/th/id/OIP.hHg6ApRWtij6di4UDJJ--AHaLH?w=204&h=306&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+        imageUrl: this.getRandomImageUrl()
       }      
 
       conts.push(cont);
     }
     this.contents.set(conts)
   }
+  
 
+  getRandomImageUrl(): string {
+
+    let randomIndex: number = Math.floor(Math.random() * this.imageUrls.length);
+    return this.imageUrls[randomIndex];
+  }
 }
